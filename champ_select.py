1559af945e7e7845654ea3ff524b3ff7699fea13
Eric Grimaldi <EricAndrewGrimaldi@Gmail.com>
2022-05-03 16:26:36 -0400

    :)

--- champ_select.py
+++ champ_select.py
@@ -2,8 +2,7 @@ from lcu_driver import Connector
 from score_calculator import Calculator
 from utils import wait_for_task
 
-# I know, global state is extremely gross
-# but I honestly could not be fucked to rewrite this as a class...
+# TODO rewrite as a class this with Willump
 
 connector = Connector()
 calc = Calculator()
